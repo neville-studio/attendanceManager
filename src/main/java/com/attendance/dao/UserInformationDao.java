b832be4534eeadd0a14efe2cb1e0cfe18af2ca85
WIndows <2840772674@qq.com>
2022-12-13 00:41:04 +0800

    Changes1

--- src/main/java/com/attendance/dao/UserInformationDao.java
+++ src/main/java/com/attendance/dao/UserInformationDao.java
@@ -16,13 +16,55 @@ public class UserInformationDao {
             preStmt.setString(1, userInfo.getAccount());
             preStmt.setString(2, userInfo.getName());
             preStmt.setString(3, userInfo.getDepartment());
-            preStmt.setString(4,userInfo.getDepartment());
+            preStmt.setString(4,userInfo.getDegree());
             preStmt.setBoolean(5,userInfo.isSex());
             preStmt.setString(6, userInfo.getWork());
             preStmt.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }finally
+        {
+            DB.close(conn,null);
         }
 
+    }
+    public static void editInfo(UserInformation userInfo)
+    {
+        Connection conn = DB.getConnection();
+        try {
+            PreparedStatement preStmt = conn.prepareStatement("SET name=?,department=?,degree=?,sex=?,work=? WHERE account=?");
+            preStmt.setString(6, userInfo.getAccount());
+            preStmt.setString(1, userInfo.getName());
+            preStmt.setString(2, userInfo.getDepartment());
+            preStmt.setString(3,userInfo.getDegree());
+            preStmt.setBoolean(4,userInfo.isSex());
+            preStmt.setString(5, userInfo.getWork());
+            preStmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally
+        {
+            DB.close(conn,null);
+        }
+    }
+
+    public static void deleteInfo(UserInformation userInfo)
+    {
+        Connection conn = DB.getConnection();
+        try {
+            PreparedStatement preStmt = conn.prepareStatement("DELETE name=?,department=?,degree=?,sex=?,work=? WHERE account=?");
+            preStmt.setString(6, userInfo.getAccount());
+            preStmt.setString(1, userInfo.getName());
+            preStmt.setString(2, userInfo.getDepartment());
+            preStmt.setString(3,userInfo.getDegree());
+            preStmt.setBoolean(4,userInfo.isSex());
+            preStmt.setString(5, userInfo.getWork());
+            preStmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally
+        {
+            DB.close(conn,null);
+        }
     }
 }
