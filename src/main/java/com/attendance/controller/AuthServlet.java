7d7f6baa5b0e7bf0e0671e18c26242ead486e93b
王徐鹏 <2840772674@qq.com>
2022-12-13 13:24:25 +0800

    Changes1

--- src/main/java/com/attendance/controller/AuthServlet.java
+++ src/main/java/com/attendance/controller/AuthServlet.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 public class AuthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain");
+        response.getOutputStream().print("{\"status\":\"不支持此请求方式！\"}");
     }
 
     @Override
@@ -22,10 +24,15 @@ public class AuthServlet extends HttpServlet {
         int status =UserDao.Login(new User(account,password,0));
         if(status<=0)
         {
+            response.sendError(200);
+            response.setContentType("text/plain");
+
             response.getOutputStream().println("{\"status\":\"登录失败，用户名或密码错误\"}");
         }else {
             request.getSession().setAttribute("account",account);
-            request.getSession().setAttribute("user_",account);
+            request.getSession().setAttribute("user_type",status);
+            response.sendRedirect("");
         }
+
     }
 }
