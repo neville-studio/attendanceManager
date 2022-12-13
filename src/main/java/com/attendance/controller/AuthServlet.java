defbf3f4e362de3f76846e9eb1fdd85a61e99c03
WIndows <2840772674@qq.com>
2022-12-13 10:44:31 +0800

    Changes1

--- src/main/java/com/attendance/controller/AuthServlet.java
+++ src/main/java/com/attendance/controller/AuthServlet.java
@@ -5,11 +5,11 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "AuthServlet", value = "/AuthServlet")
+@WebServlet(name = "AuthServlet", value = "/Auth")
 public class AuthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        s
     }
 
     @Override
